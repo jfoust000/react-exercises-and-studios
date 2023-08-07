b79f057ee3d641dfd73c624e6ad36508c698579e
Joel Foust <joel.foust@yahoo.com>
2023-08-07 13:57:18 -0400

    React Part 1 Exercise

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Books I Read This Month";
+   let book1 = "https://m.media-amazon.com/images/I/715anDaCOvL._AC_UL800_FMwebp_QL65_.jpg";
+   let book2 = "https://m.media-amazon.com/images/I/91+AC9AYVWL._AC_UL800_FMwebp_QL65_.jpg";
+   let book3 = "https://m.media-amazon.com/images/I/91afIYwE9BL._AC_UL800_FMwebp_QL65_.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Out Of Nowhere by Sandra Brown" />
+         <img src={book2} alt="The Edge by David Baldacci" />
+         <img src={book3} alt="The Exchange by John Grisham" />
       </div>      
    );
 }
