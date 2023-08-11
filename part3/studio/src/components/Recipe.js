e567e1dbd62491f3e5889ee7d09618ca862f811b
Joel Foust <joel.foust@yahoo.com>
2023-08-11 10:41:58 -0400

    REACT Part 3 Studio Recipe

--- part3/studio/src/components/Recipe.js
+++ part3/studio/src/components/Recipe.js
@@ -1,21 +1,23 @@
 const RecipeAuthor = () => {
-   let authorLink = "";
-   let authorPhoto = "";
-   let authorName = "";
+   let authorLink = "https://www.bonappetit.com/contributor/amiel-stanek";
+   let authorPhoto = "https://assets.bonappetit.com/photos/5d001f309ffc67792fc6f7ed/1:1/w_320,c_limit/Ba_Staff_Portrait_Amiel.jpg";
+   let authorName = "Amiel Stanek";
 
    return (
       <div>
          <img src={authorPhoto} alt = "" style={{objectFit: "contain", borderRadius: "50%"}} />
          <div>
             <h3>{authorName}</h3>
-            <a href={authorLink}></a> 
+            <a href={authorLink}>{authorLink}</a> 
          </div>
       </div>
    );
 }
 
 const RecipeIngredients = () => {
-   const ingredients = [];
+   const ingredients = ["Kosher salt", "2 1½-lb. live lobsters", 
+   "1 medium onion, coarsely chopped", "2 celery stalks, trimmed, coarsely chopped", "3 garlic cloves, peeled, smashed"];
+
    return(
       <div>
          <h3>Recipe Ingredients</h3>
@@ -31,11 +33,15 @@ const RecipeIngredients = () => {
 }
 
 const RecipeDescription = () => {
+
+   let recipeTitle = "Lobster Pasta";
+   let recipeDescription = "This recipe is no small amount of work, so be sure to make it for the people you really, really like. The pro move is to break up the process over two days—that means cooking the lobsters, picking the meat, and infusing the cream on the first day, which leaves cooking the pasta and heating everything back up to serve on the second. And no, you can't just buy already picked lobster meat; you need the shells to infuse the sauce with tons of flavor. You can, however, ask your fishmonger to steam the lobsters for you, which many will do for free if you ask. No judgie.";
+
    return (
       <div> 
          <div>
-            <h1></h1>
-            <p></p>
+            <h1>{recipeTitle}</h1>
+            <p>{recipeDescription}</p>
          </div>
          <div className="recipePhotoBlock">
             <RecipeIngredients />
@@ -47,7 +53,7 @@ const RecipeDescription = () => {
 
 const RecipePhoto = () => {
    return (
-      <img src="" alt="" className="imageUpdates"/>
+      <img src="https://assets.bonappetit.com/photos/5de7e495b79e20000879d72a/1:1/w_1920,c_limit/Amiel-Lobster-Pasta-Lede-1.jpg" alt="" className="imageUpdates"/>
    );
 }
 
